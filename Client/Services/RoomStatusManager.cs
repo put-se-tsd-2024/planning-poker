abdcfbfa78c5fb0b5bc9c486cc76e6f2d530397d
andreistaravoitau <andrei.v.staravoitau@gmail.com>
2024-04-08 23:27:32 +0200

    add client-side

---
+++ Client/Services/RoomStatusManager.cs
@@ -0,0 +1,11 @@
+﻿using PlanningPoker.Shared;
+
+namespace PlanningPoker.Client.Services
+{
+    public class RoomStatusManager
+    {
+        public Player Player { get; set; } = new Player();
+        public Room Room { get; set; } = new Room();
+        public bool IsStatusReady => !(string.IsNullOrWhiteSpace(Player.Name) || string.IsNullOrWhiteSpace(Room.Id));
+    }
+}
