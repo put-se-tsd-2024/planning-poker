52d13f2db6be4731abe6e3b858a76bd7f3f2b19c
kooogi <bartosz.chwilkowski00@gmail.com>
2024-05-04 12:31:17 +0200

    Added code structure and files for UserStories

---
+++ Shared/UserStory.cs
@@ -0,0 +1,11 @@
+﻿namespace PlanningPoker.Shared
+{
+    public class UserStory
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        // Other properties needed for the user story?
+    }
+}
